dfd861c6e24a031598bd721c0c91a6762888b690
jangmoonseok <cacao621@naver.com>
2023-01-04 22:24:42 +0900

    프로젝트 생성

---
+++ src/components/COMMON/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input class="textField" type="text" placeholder="검색어를 입력하세요."/>
+        <button type="button">검색</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    components: {},
+    props: {},
+    data() {
+        return {}
+    },
+    create(){},
+    computed(){},
+    watch(){},
+    mounted(){},
+    methods: {},
+}
+</script>
+
+<style lang='scss' scoped></style>
